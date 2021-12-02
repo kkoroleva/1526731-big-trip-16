23fcd4edaf927a409267d05183e70c4878e665ed
kkoroleva <korolevakarinka22@gmail.com>
2021-12-02 17:25:29 +0300

    Installs and configs webpack

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+module.exports = {
+  entry: "main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devtool: 'source-map'
+}
